5797185d3d2607517a4e86042ad0bb28f37f0477
unknown <paulinamonstaviciute@gmail.com>
2019-10-29 17:36:02 +0200

    Praktika

--- index.php
+++ index.php
@@ -3,14 +3,9 @@
         <title>Komentarai></title>
     </head>
     <body>
-        <h1>
-            <?php Dainų tekstai ?>
-        </h1> 
-        <h2>
-            <?php Paragrafo Daina ?></h2>
-
-        <p> <?php Aš toks vienišas jaunas paragrafas... ?></p>
-        <!--
-       <p> <?php Mindau svtimo kodo gatves... ?></p>  -->
+        <h1>Dainų tekstai</h1> 
+        <h2>Paragrafo Daina</h2>
+        <p>Aš toks vienišas jaunas paragrafas...</p>
+        <p><?php // Mindau svtimo kodo gatves... // ?></p> 
     </body>
 </html>
