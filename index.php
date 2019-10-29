c54cfb14a4c099b0cff268a79e5a76f27d59c22d
unknown <paulinamonstaviciute@gmail.com>
2019-10-29 15:01:49 +0200

    Praktika su PHP

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-print 'veikia!';
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Aš valdau HTML!</title>
+  </head>
+  <body>
+    <h1>Paulina - HTML asėĄ</h1>
+    <p>Moku kurti statinį turinį.</p>
+  </body>
+</html>
