3ab92c0d6e74eccb2497e190648c3e703bf1720a
unknown <paulinamonstaviciute@gmail.com>
2019-10-29 15:51:51 +0200

    praktika su ...

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title> Aš valdau HTML!</title>
+    <title> Aš valdau HTML <?php print 'ir PHP' ?> !</title>
   </head>
   <body>
-    <h1>Paulina - HTML asėĄ</h1>
-    <p>Moku kurti statinį turinį.</p>
+    <h1><b>Paulina - HTML <?php print 'ir PHP' ?> asė!</b></h1>
+    <p>Kuriu dinaminį turinį.
+       Atidaryta: <?php date ('Y-m-d H:i:s');
+       print date ('Y-m-d H:i:s')?>
+    </p>
   </body>
 </html>
