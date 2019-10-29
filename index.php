79f79f74ba554fbaa1105ecc5be4006efbfbdaa5
unknown <paulinamonstaviciute@gmail.com>
2019-10-29 16:33:08 +0200

    Praktika

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title> Aš valdau HTML <?php print 'ir PHP' ?> !</title>
+    <title><?php print 'Aš,'. date ('&day'). 'PHP';?> !</title>
   </head>
   <body>
-    <h1><b>Paulina - HTML <?php print 'ir PHP' ?> asė!</b></h1>
-    <p>Kuriu dinaminį turinį.
-       Atidaryta: <?php date ('Y-m-d H:i:s');
-       print date ('Y-m-d H:i:s')?>
+    <h1><b>Paulina - HTML <?php print 'ir PHP';?> asė jau nuo <?php print date ('Y'). 'metų' ?></b></h1>
+    <p>Viskas prasidėjo <?php print date ('m'). date('d');?>
     </p>
   </body>
 </html>
+<?php ?>
