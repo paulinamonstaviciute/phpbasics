5ee9f49d767d48ffc5e4c1b91bc7dc8d8e7478a1
unknown <paulinamonstaviciute@gmail.com>
2019-10-29 17:30:34 +0200

    Praktika su PHP

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title><?php print 'Aš,'. date ('&day'). 'PHP';?> !</title>
-  </head>
-  <body>
-    <h1><b>Paulina - HTML <?php print 'ir PHP';?> asė jau nuo <?php print date ('Y'). 'metų' ?></b></h1>
-    <p>Viskas prasidėjo <?php print date ('m'). date('d');?>
-    </p>
-  </body>
+<html>
+    <head>
+        <title>//Komentarai//></title>
+    </head>
+    <body>
+        <h1>
+            <?php //Dainų tekstai//?>
+        </h1> 
+        <h2> 
+            <?php //Paragrafo Daina// ?>
+        </h2>
+        <p> <?php //Aš toks vienišas jaunas paragrafas...//  ?></p>
+        <p> <?php //Mindau svtimo kodo gatves...//  ?></p>
+    </body>
 </html>
-<?php ?>
