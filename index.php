a99c911a1c4a0d2c1eaa18b692fef882ce9697f3
unknown <paulinamonstaviciute@gmail.com>
2019-10-31 15:07:39 +0200

    variable

--- index.php
+++ index.php
@@ -1,30 +1,24 @@
+<?php
+$string = 'Labas PHP!';
+$integer = 153;
+$boolean = true;
+$null = null;
+
+$li_1 = "$string";
+$li_2 = "$integer";
+$li_3 = "$boolean";
+$li_4 = "$null";
+?>
 <html>
     <head>
-        <title>Bomba</title>
-        <style>
-            body {
-                display:flex;
-                flex-direction:column;
-                align-items:center;
-            }
-            .bomba{
-                height: <?php print date('1s'); ?>px;
-                width: <?php print date('1s'); ?>px;
-                background-image: url("https://mpng.pngfly.com/20180802/esc/kisspng-vector-graphics-explosion-clip-art-bomb-image-5b63381813fbc0.4716718515332290800819.jpg");
-                background-size:cover;
-            }
-            p {
-                text-align: center;
-            } 
-            .bomba-00{
-                width: 1000px;
-                height: 1000px;
-                background-image: url(https://g3.dcdn.lt/images/pix/atomines-bombos-sprogimas-81021179.jpg);  
-            }
-        </style>
+        <title>PHP</title>
     </head>
     <body>
-        <div class="bomba bomba-<?php print date('s')?>"></div>
-        <p><?php print date('s') ?></p>
+        <ul>
+            <li><?php print $li_1 ?></li>
+            <li><?php print $li_2 ?></li>
+            <li><?php print $li_3 ?></li>
+            <li><?php print $li_4 ?></li>
+        </ul>
     </body>
 </html>
