5cfa24687411325d1179c6346b250eeb2924804d
unknown <paulinamonstaviciute@gmail.com>
2019-10-31 15:55:21 +0200

    PHP

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
-$img_src = 'https://seethefullpicture.ca/wp-content/uploads/2019/06/Alcon_SeeTheFullPicture_Website_1901x11252.jpg';
+$integer_1 = rand(1, 100);
+$integer_2 = rand(101, 200);
+$integer_3 = rand(201, 300);
+$integer_4 = rand(301, 400);
+
+$h1 = " $integer_1 ";
+$h2 = " $integer_2 ";
+$h3_1 = " $integer_3 ";
+$h3_2 = " $integer_4 ";
 ?>
 <html>
     <head>
         <title>PHP</title>
-        <style>
-            img{width: 200px;
-                height: 200px;
-            }
-        </style>
     </head>
     <body>
-        <img src="<?php print $img_src; ?>">
-        <img src="<?php print $img_src; ?>">
-        <img src="<?php print $img_src; ?>">
-        <img src="<?php print $img_src; ?>">
+        <h1>Skolos skaičiuoklė<?php print $h1; ?></h1>
+        <h2>Jei paėmei<?php print $h2; ?> jievru</h2>
+        <h3>Su dviem kabančiais grąžinsi<?php print $h3_1; ?></h3>
+        <h3>Su vienu kabančiu grąžinsi<?php print $h3_2; ?></h3>
     </body>
 </html>
