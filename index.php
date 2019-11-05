394a9586e133090613271191640cc23207fb0687
unknown <paulinamonstaviciute@gmail.com>
2019-11-05 15:26:29 +0200

    ciklai

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 <?php
-$integer_1 = rand(1, 100);
-$integer_2 = rand(101, 200);
-$integer_3 = rand(201, 300);
-$integer_4 = rand(301, 400);
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+$katasuniai = 0;
 
-$h1 = " $integer_1 ";
-$h2 = " $integer_2 ";
-$h3_1 = " $integer_3 ";
-$h3_2 = " $integer_4 ";
+for ($x = 1; $x <= $kates; $x++) {
+    for ($y = 1; $y <= $sunys; $y++) {
+        $pavyko = rand(0, 1);
+        if ($pavyko) {
+            $katasuniai++;
+            break;
+        }
+    }
+}
+$h1 = 'Katašunių išeiga';
+$h2 = " Dalyvavo $kates katės ir $sunys šunys";
+$h3 = "Katašunių išeiga: $katasuniai";
 ?>
 <html>
     <head>
-        <title>PHP</title>
     </head>
     <body>
-        <h1>Skolos skaičiuoklė<?php print $h1; ?></h1>
-        <h2>Jei paėmei<?php print $h2; ?> jievru</h2>
-        <h3>Su dviem kabančiais grąžinsi<?php print $h3_1; ?></h3>
-        <h3>Su vienu kabančiu grąžinsi<?php print $h3_2; ?></h3>
+        <h1><?php print $h1; ?></h1>
+        <h2><?php print $h2; ?></h2>
+        <h3><?php print $h3; ?></h3>
     </body>
 </html>
