b2d651b588b8afc56be2a99e0b185513b7b8ebc5
unknown <paulinamonstaviciute@gmail.com>
2019-11-07 15:23:27 +0200

    php

--- index.php
+++ index.php
@@ -5,22 +5,25 @@ $product = [
         'vardas' => 'Petras',
         'pavarde' => 'Pizdžius',
         'items' => [
-            'busena' => 'prap*stas',
-            'pavadinimas' => 'telefonas',
+            'item_1' => [
+                'busena' => 'prap*stas',
+                'pavadinimas' => 'telefonas'],
         ],
     ],
     [
         'vardas' => 'Tomas',
         'pavarde' => 'Ablomas',
         'items' => [
-            'busena' => 'naudojamas',
-            'pavadinimas' => 'buljonas'],],
+            'item_1' => [
+                'busena' => 'naudojamas',
+                'pavadinimas' => 'buljonas'],],],
     [
         'vardas' => 'Ana',
         'pavarde' => 'Shitkova',
         'items' => [
-            'busena' => 'pasibaigęs',
-            'pavadinimas' => 'rulonas']
-    ],];
+            'item_1' => [
+                'busena' => 'pasibaigęs',
+                'pavadinimas' => 'rulonas']
+        ],],];
 
 var_dump($product);
