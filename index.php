e91f21da7585c4d43ea1d3c1109f650a5b7cdecd
unknown <paulinamonstaviciute@gmail.com>
2019-11-07 16:01:24 +0200

    php

--- index.php
+++ index.php
@@ -1,29 +1,27 @@
 <?php
 
 $product = [
-    [
-        'vardas' => 'Petras',
-        'pavarde' => 'Pizdžius',
-        'items' => [
-            'item_1' => [
-                'busena' => 'prap*stas',
-                'pavadinimas' => 'telefonas'],
-        ],
+    'pirma_lentyna' => [
+        'produktai' =>
+        ['produktas_1' => 'Kiaušiniai',
+            'produktas_1_kiekis' => 3],
+        ['produktas_2' => 'Grietinė',
+            'produktas_2_kiekis' => 1],
+        ['produktas_3' => 'Jogurtas',
+            'produktas_3_kiekis' => 2],
+        ['produktas_4' => 'Žuvis',
+            'produktas_4_kiekis' => 0],
     ],
-    [
-        'vardas' => 'Tomas',
-        'pavarde' => 'Ablomas',
-        'items' => [
-            'item_1' => [
-                'busena' => 'naudojamas',
-                'pavadinimas' => 'buljonas'],],],
-    [
-        'vardas' => 'Ana',
-        'pavarde' => 'Shitkova',
-        'items' => [
-            'item_1' => [
-                'busena' => 'pasibaigęs',
-                'pavadinimas' => 'rulonas']
-        ],],];
+    'antra_lentyna' => [
+        'produktai' =>
+        ['produktas_1' => 'Pienas',
+            'produktas_1_kiekis' => 1],
+        ['produktas_2' => 'Kefyras',
+            'produktas_2_kiekis' => 0],
+        ['produktas_3' => 'Degtinė',
+            'produktas_3_kiekis' => 2],
+        ['produktas_4' => 'Kečupas',
+            'produktas_4_kiekis' => 2],
+    ],];
 
 var_dump($product);
