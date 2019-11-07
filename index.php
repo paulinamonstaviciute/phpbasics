695248712f5d63320900b265bd91cbbfecf49c07
unknown <paulinamonstaviciute@gmail.com>
2019-11-07 16:04:38 +0200

    php

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
 
-$product = [
+$spinta = [
     'pirma_lentyna' => [
         'produktai' =>
-        ['produktas_1' => 'Kiaušiniai',
-            'produktas_1_kiekis' => 3],
-        ['produktas_2' => 'Grietinė',
-            'produktas_2_kiekis' => 1],
-        ['produktas_3' => 'Jogurtas',
-            'produktas_3_kiekis' => 2],
-        ['produktas_4' => 'Žuvis',
-            'produktas_4_kiekis' => 0],
+        ['produktas' => 'Kiaušiniai',
+            'produktas_kiekis' => 3],
+        ['produktas' => 'Grietinė',
+            'produktas_kiekis' => 1],
+        ['produktas' => 'Jogurtas',
+            'produktas_kiekis' => 2],
+        ['produktas' => 'Žuvis',
+            'produktas_kiekis' => 0],
     ],
     'antra_lentyna' => [
         'produktai' =>
-        ['produktas_1' => 'Pienas',
-            'produktas_1_kiekis' => 1],
-        ['produktas_2' => 'Kefyras',
-            'produktas_2_kiekis' => 0],
-        ['produktas_3' => 'Degtinė',
-            'produktas_3_kiekis' => 2],
-        ['produktas_4' => 'Kečupas',
-            'produktas_4_kiekis' => 2],
+        ['produktas' => 'Pienas',
+            'produktas_kiekis' => 1],
+        ['produktas' => 'Kefyras',
+            'produktas_kiekis' => 0],
+        ['produktas' => 'Degtinė',
+            'produktas_kiekis' => 2],
+        ['produktas' => 'Kečupas',
+            'produktas_kiekis' => 2],
     ],];
 
-var_dump($product);
+var_dump($spinta);
