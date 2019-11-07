84a7d1be8cf99a165548388e1f9c7f4b6495091c
unknown <paulinamonstaviciute@gmail.com>
2019-11-07 15:00:00 +0200

    php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
 $product = [
-    '1' => [
+    1 => [
         'kiekis' => 'du',
         'dydis' => 'dideli',
         'pavadinimas' => 'kiausiniai'],
-    '2' => [
+    2 => [
         'kiekis' => 'vienas',
         'dydis' => 'mazas',
         'pavadinimas' => 'baklazanas'],
-    '3' =>
+    3 =>
     ['kiekis' => 'vienas',
         'dydis' => 'dideli',
         'pavadinimas' => 'grietines indelis']];
