ab34841900e0adbbe166bc531b47e920dc3cb7f9
unknown <paulinamonstaviciute@gmail.com>
2019-11-07 17:07:04 +0200

    php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 
 $kelias = [
-    ['sukimo_krytis' => 'į_dešinę',
+    ['sukimo_kryptis' => 'i_desine',
         'sukimo_kampas' => 90],
-    ['sukimo_krytis' => 'į_kairę',
+    ['sukimo_kryptis' => 'i_kaire',
         'sukimo_kampas' => 45],
-    ['sukimo_krytis' => 'į_dešinę',
+    ['sukimo_kryptis' => 'i_desine',
         'sukimo_kampas' => 160],
-    ['sukimo_krytis' => 'į_kairę',
+    ['sukimo_kryptis' => 'i_kaire',
         'sukimo_kampas' => 160],
-    ['sukimo_krytis' => 'į_dešinę',
+    ['sukimo_kryptis' => 'i_desine',
         'sukimo_kampas' => 180],
-    ['sukimo_krytis' => 'į_dešinę',
+    ['sukimo_kryptis' => 'i_desine',
         'sukimo_kampas' => 40],
 ];
 var_dump($kelias);
